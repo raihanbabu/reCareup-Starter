df05ca6fd27d1c5f79a858c10b8e3cb706e66fd6
Raihan <aburaihankabir@gmail.com>
2023-09-05 09:48:26 +0600

    add php file

---
+++ recareup-starter.php
@@ -0,0 +1,15 @@
+/*
+ * Plugin Name: reCareup Starter
+ * Plugin URI:        https://github.com/raihanbabu/reCareup-Starter
+ * Description:       This is personal plugin
+ * Version:           0.0.1
+ * Requires at least: 6.3.1
+ * Requires PHP:      8.2
+ * Author:            Raihan
+ * Author URI:        https://linkedin.com/in/raihanbabu
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/raihanbabu/reCareup-Starter
+ * Text Domain:       recareup-starter
+ * Domain Path:       /languages
+ */
